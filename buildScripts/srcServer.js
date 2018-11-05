7b08e590c328c3a4fed0e99011145e6950882e88
Sebastian Secara <sebi_secara@yahoo.com>
2018-11-05 22:13:40 +0000

    Linting

--- buildScripts/srcServer.js
+++ buildScripts/srcServer.js
@@ -2,7 +2,9 @@ import express from 'express'
 import path from "path";
 import open from 'open';
 import webpack from 'webpack';
-import config from '../webpack.config.dev'
+import config from '../webpack.config.dev';
+
+/* eslint-disable no-console */
 
 const port = 3000;
 const app = express();
--- src/index.js
+++ src/index.js
@@ -4,6 +4,6 @@ import numeral from 'numeral';
 
 const courseValue = numeral(1000).format('$0,0.00');
 debuger
-console.log(`I would pay ${courseValue} for this course!`);
+console.log(`I would pay ${courseValue} for this course!`); // eslint-disable-line no-comment
 
 
